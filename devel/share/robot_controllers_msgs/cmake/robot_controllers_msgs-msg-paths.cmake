f38ab7e16c8e055a2ee7b3c03b68b66dcc4b52f3
Masatoshi Chang <masatoshi.chang@gmail.com>
2017-04-15 16:00:36 -0400

    Initial commit

---
+++ devel/share/robot_controllers_msgs/cmake/robot_controllers_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(robot_controllers_msgs_MSG_INCLUDE_DIRS "/home/masatoshichang/robotics/fetch_ws/src/robot_controllers/robot_controllers_msgs/msg;/home/masatoshichang/robotics/fetch_ws/devel/share/robot_controllers_msgs/msg")
+set(robot_controllers_msgs_MSG_DEPENDENCIES actionlib_msgs;std_msgs)
